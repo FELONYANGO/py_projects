95248c67594f1c3c8008f634375f038dae053733
FELONYANGO <felixonyango951@gmail.com>
2021-11-23 08:40:09 +0300

    second commit

---
+++ emoji _converter.py
@@ -0,0 +1,12 @@
+#reguesting the user to enter a message
+message= input(">")
+word=message.split(' ')#split method to split the give word
+
+#emoji conveter stored as dictionary
+emoji={
+
+    :)=""
+}
+
+print(word)#printig the splitted message
+
