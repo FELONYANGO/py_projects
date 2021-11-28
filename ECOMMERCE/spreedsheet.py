1e0c5581bafb7c295bb1272bef3ab44eeaa28650
FELONYANGO <felixonyango951@gmail.com>
2021-11-28 23:08:46 +0300

    first commit

--- ECOMMERCE/spreedsheet.py
+++ ECOMMERCE/spreedsheet.py
@@ -5,7 +5,15 @@ wk = xl.load_workbook("transactions.xlsx")
 sheet = wk["Sheet1"]
 cell= sheet["a1"]
 
-for  row in range(1,sheet.max_row + 1):#looping through each row in the sheet 1
-    print(row)
+print(cell.value)
+
+for  row in range(2,sheet.max_row + 1):#looping through each row in the sheet 1
+  
     cell = sheet.cell(row  ,3)
-    print(cell)
+  
+    corected_price = float(cell.value)*0.9
+    corrected_price_cell = sheet.cell(row,4)
+    corrected_price_cell.value=corected_price
+
+
+wk.save("transactions2.xlsx")
